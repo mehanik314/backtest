54292a4505f376b2e7fc5c8147f2f0b5f06d947d
mehanik314 <vihr76@yandex.ru>
2024-03-18 15:54:06 +0300

    'b'

--- backcrm/settings.py
+++ backcrm/settings.py
@@ -51,12 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS: True
+
 CORS_ALLOWED_ORIGINS = ["https://resilient-tarsier-b97d55.netlify.app/", "http://localhost:3000"]
-CORS_ORIGIN_WHITELIST = [
-    "https://resilient-tarsier-b97d55.netlify.app/",
-    "http://localhost:3000"
-]
+
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
